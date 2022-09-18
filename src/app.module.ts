72b74bffa406ebcc2a636bb3e2cf3d344358ff91
Joao <joaoguilhermefagundes@gmail.com>
2022-09-17 21:23:43 -0300

    connection with mongodbAtlas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { JogadoresModule } from './jogadores/jogadores.module';
 
 @Module({
-  imports: [JogadoresModule],
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://admin:3mJoBl1Jac9gHQiw@cluster0.eaffz6i.mongodb.net/smartrankingapi',
+    { useNewUrlParser: true, useCreateIndex: true, UseUnifiedTopology: true, useFindAndModify: false }),
+    JogadoresModule],
   controllers: [],
   providers: [],
 })
